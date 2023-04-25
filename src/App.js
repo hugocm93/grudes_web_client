1574579fa0fdfbc546e21ff4729fdf5ff862d42c
Hugo Machado <hugocm93@Hugos-MacBook.lan>
2023-04-25 01:09:44 -0400

    Implementa tabela de receitas     - deixa tabela genérica para reúso

--- src/App.js
+++ src/App.js
@@ -23,24 +23,18 @@ function to_form_data(obj)
     return formData;
 }
 
-function List({ items, removeCbk }) {
+function Table({ columns, rows}) {
   return (
     <table>
       <thead>
         <tr>
-          <th>Nome</th>
-          <th></th>
+        {columns.map((column) => (<th>{column.name}</th>))}
         </tr>
       </thead>
       <tbody>
-        {items.map((row, index) => (
+        {rows.map((row) => (
           <tr key={row.id}>
-            <td><input
-                type="text"
-                placeholder="Nome"
-                onChange = {(event) => {row.name = event.target.value;}}>
-            </input></td>
-            <button className = "RemoveSubstituteBtn" onClick = {() => {removeCbk(row.id)}}> X </button>
+            {columns.map((column) => (<td>{column.get(row)}</td>))}
           </tr>
         ))}
       </tbody>
@@ -100,11 +94,12 @@ function Recipes()
     return (
         <div className="Recipes">
             <h1> Receitas </h1>
+            <RecipesArea />
         </div>
     );
 }
 
-function AddIngredientArea()
+function IngredientArea()
 {
     const [name, setName] = useState("");
     const [substitutes, setSubstitutes] = useState([]);
@@ -143,12 +138,139 @@ function AddIngredientArea()
     function remove_substitute(id)
     { setSubstitutes(substitutes.filter((s) => { console.log(s.name); return s.id !== id; })); }
 
+    const columns = [
+        {
+            name: "Substituto",
+            get: (row) => {
+                return (
+                    <input
+                        type="text"
+                        placeholder="Nome"
+                        onChange = {(event) => {row.name = event.target.value;}}>
+                    </input>
+                );
+            }
+        },
+        {
+            name: "X",
+            get: (row) => {
+                return (
+                    <button className = "RemoveSubstituteBtn" onClick = {() => {remove_substitute(row.id)}}> X </button>
+                );
+            }
+        }
+    ];
+
     return (
-        <div className="AddIngredientArea">
+        <div className="IngredientArea">
             <input type = "text" placeholder = "Nome" value = {name} onChange = {onChangeCbk}></input>
             <button className = "Save" onClick={save_ingredient}> Salvar </button>
             <button className = "AddSubstituteBtn" onClick = {add_substitute_item}> Adicionar </button>
-            <List items = {substitutes} removeCbk = {remove_substitute}/>
+            <Table columns = {columns} rows = {substitutes} />
+        </div>
+    );
+}
+
+function RecipesArea()
+{
+    const [name, setName] = useState("");
+    const [ingredients, setIngredients] = useState([]);
+    const [instructions, setInstructions] = useState([]);
+
+    function onNameChangeCbk(event) {
+        setName(event.target.value);
+    }
+
+    function onInstructionsChangeCbk(event) {
+        setInstructions(event.target.value);
+    }
+
+    async function save_recipe()
+    {
+        const recipe = {
+            name: name
+            //ingredients: ingredients.map(substitute => substitute.name)
+        };
+
+        fetch(url + "/recipe",
+        {
+            method: "post",
+            body: to_form_data(recipe)
+        })
+        .then((response) => response.json())
+        .then((data) => {console.log(data)})
+        .catch((error) => {console.error("Error:", error)});
+    }
+
+    function add_ingredient_item() {
+        const newIngredient = {
+            name: "ingrediente",
+            quantity: 0,
+            unit: "xícara",
+            id: uuidv4()
+        };
+
+        const newIngredients = [...ingredients, newIngredient];
+        setIngredients(newIngredients);
+    }
+
+    function remove_ingredient(id)
+    { setIngredients(ingredients.filter((s) => { console.log(s.name); return s.id !== id; })); }
+
+    const columns = [
+        {
+            name: "Ingrediente",
+            get: (row) => {
+                return (
+                    <input
+                        type="text"
+                        placeholder="Nome"
+                        onChange = {(event) => {row.name = event.target.value;}}>
+                    </input>
+                );
+            }
+        },
+        {
+            name: "Quantidade",
+            get: (row) => {
+                return (
+                    <input
+                        type = "number"
+                        min = {0}
+                        onChange = {(event) => {row.quantity = event.target.value;}}>
+                    </input>
+                );
+            }
+        },
+        {
+            name: "Unidade",
+            get: (row) => {
+                return (
+                    <input
+                        type = "text"
+                        placeholder = "xícara"
+                        onChange = {(event) => {row.unit = event.target.value;}}>
+                    </input>
+                );
+            }
+        },
+        {
+            name: "X",
+            get: (row) => {
+                return (
+                    <button className = "RemoveIngredientBtn" onClick = {() => {remove_ingredient(row.id)}}> X </button>
+                );
+            }
+        }
+    ];
+
+    return (
+        <div className="RecipeArea">
+            <input type = "text" placeholder = "Nome" value = {name} onChange = {onNameChangeCbk}></input>
+            <input type = "text" placeholder = "Instruções" value = {instructions} onChange = {onInstructionsChangeCbk}></input>
+            <button className = "Save" onClick={save_recipe}> Salvar </button>
+            <button className = "AddIngredientBtn" onClick = {add_ingredient_item}> Adicionar </button>
+            <Table columns = {columns} rows = {ingredients} />
         </div>
     );
 }
@@ -158,7 +280,7 @@ function Ingredients()
     return (
         <div className="Ingredients">
             <h1> Ingredientes </h1>
-            <AddIngredientArea />
+            <IngredientArea />
         </div>
     );
 }
