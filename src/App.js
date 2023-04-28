39b63fd8d7694e0896b756b9a52519e406d72254
Hugo Machado <hugocm93@Hugos-MacBook.lan>
2023-04-28 16:40:47 -0400

    Implementa listagem e remoção de ingredientes

--- src/App.js
+++ src/App.js
@@ -34,15 +34,12 @@ async function get_recipes_impl(name, ingredients, setRecipes)
             url_ = url_ + `&${new URLSearchParams({ingredients: ingredient.name}).toString()}`
     });
 
-    console.log(url_);
-
     return fetch(url_,
     {
         method: "get",
     })
     .then((response) => response.json())
     .then((data) => {
-        console.log(data.recipes);
         if(data.recipes)
         {
             data.recipes.forEach((recipe) => { recipe.id = uuidv4(); });
@@ -54,8 +51,27 @@ async function get_recipes_impl(name, ingredients, setRecipes)
     .catch((error) => {console.error("Error:", error)});
 }
 
-function bind(setName) {
-    return (event) => { setName(event.target.value); }
+async function get_ingredients_impl()
+{
+    return fetch(url + "/ingredients",
+    {
+        method: "get",
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        if(data.ingredients)
+        {
+            data.ingredients.forEach((recipe) => { recipe.id = uuidv4(); });
+            return data.ingredients;
+        }
+        else
+            return [];
+    })
+    .catch((error) => {console.error("Error:", error)});
+}
+
+function bind(setter) {
+    return (event) => { setter(event.target.value); }
 }
 
 function Table({ columns, rows, onClick}) {
@@ -138,6 +154,7 @@ function IngredientsTable({title, ingredients, setIngredients})
                     <input
                         type="text"
                         placeholder="Nome"
+                        value = {row.name}
                         onChange = {(event) => {row.name = event.target.value;}}>
                     </input>
                 );
@@ -222,14 +239,15 @@ function RecipesTab()
     );
 }
 
-function IngredientArea()
+function IngredientsArea()
 {
     const [name, setName] = useState("");
     const [substitutes, setSubstitutes] = useState([]);
+    const [ingredients, setIngredients] = useState([]);
 
-    function onChangeCbk(event) {
-        setName(event.target.value);
-    }
+    useEffect(() => {
+        get_ingredients_impl().then((ingredients) => (setIngredients(ingredients)));
+    }, []);
 
     async function save_ingredient()
     {
@@ -277,33 +295,66 @@ function IngredientArea()
     function remove_substitute(id)
     { setSubstitutes(substitutes.filter((s) => { return s.id !== id; })); }
 
+    function remove_ingredient(id)
+    {
+        const idx = ingredients.findIndex((s) => { return s.id === id; });
+        const found = ingredients.at(idx);
+
+        if(!window.confirm("Deseja realmente remover o ingrediente " + found.name + "?"))
+            return;
+
+        fetch(url + "/ingredient" + `?${new URLSearchParams({name: found.name}).toString()}`,
+        {
+            method: "delete",
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            get_ingredients_impl().then((ingredients) => (setIngredients(ingredients)));
+        })
+        .catch((error) => {console.error("Error:", error)});
+    }
+
     const columns = [
         {
-            name: "Substituto",
-            get: (row) => {
-                return (
-                    <input
-                        type="text"
-                        placeholder="Nome"
-                        onChange = {(event) => {row.name = event.target.value;}}>
-                    </input>
-                );
-            }
+            name: "Ingrediente",
+            get: (row) => { return (<label> {row.name} </label>); }
         },
         {
             name: "X",
             get: (row) => {
                 return (
-                    <button className = "RemoveSubstituteBtn" onClick = {() => {remove_substitute(row.id)}}> X </button>
+                    <button className = "RemoveIngredientBtn" onClick = {() => {remove_ingredient(row.id)}}> X </button>
                 );
             }
         }
     ];
 
+    function show_ingredient(name)
+    {
+        const ingredient = ingredients.find((ingredient) => (ingredient.name === name));
+        if(ingredient)
+        {
+            setName(ingredient.name);
+
+            setSubstitutes(ingredient.substitutes.map((substitute) => {
+                return {name: substitute, id: uuidv4()};
+            }));
+        }
+        else
+        {
+            setName("");
+            setSubstitutes([]);
+        }
+    }
+
+    function table_clicked(event)
+    { show_ingredient(event.target.textContent.trim()); }
+
     return (
-        <div className="IngredientArea">
+        <div className="IngredientsArea">
+            <Table columns = {columns} rows = {ingredients} onClick = {table_clicked}/>
             <div>
-                <input type = "text" placeholder = "Nome" value = {name} onChange = {onChangeCbk}></input>
+                <input type = "text" placeholder = "Nome" value = {name} onChange = {bind(setName)}></input>
                 <IngredientsTable title = "Substitutos" ingredients = {substitutes} setIngredients = {setSubstitutes}/>
             </div>
             <button className = "SaveIngredientBtn" onClick={save_ingredient}> Salvar </button>
@@ -318,8 +369,6 @@ function RecipesTable({recipes, setRecipes, selectRecipe})
         const idx = recipes.findIndex((s) => { return s.id === id; });
         const found = recipes.at(idx);
 
-        console.log(found.name);
-
         if(!window.confirm("Deseja realmente remover a receita " + found.name + "?"))
             return;
 
@@ -329,7 +378,6 @@ function RecipesTable({recipes, setRecipes, selectRecipe})
         })
         .then((response) => response.json())
         .then((data) => {
-            console.log(data);
             setRecipes(recipes.toSpliced(idx, 1)); 
         })
         .catch((error) => {console.error("Error:", error)});
@@ -374,14 +422,6 @@ function RecipesArea()
     useEffect(() => {
         get_recipes_impl("", ingredients, setRecipes);
     }, []);
-
-    function onNameChangeCbk(event) {
-        setName(event.target.value);
-    }
-
-    function onInstructionsChangeCbk(event) {
-        setInstructions(event.target.value);
-    }
 
     async function save_recipe()
     {
@@ -499,7 +539,6 @@ function RecipesArea()
                 recipe.ingredients.forEach((ingredient) => {
                     ingredient.name = ingredient.ingredient;
                     ingredient.id = uuidv4();
-                    console.log(ingredient);
                 });
                 setIngredients(recipe.ingredients);
             }
@@ -513,10 +552,10 @@ function RecipesArea()
                     <RecipesTable recipes = {recipes} setRecipes = {setRecipes} selectRecipe = {show_recipe} />
                 </div>
                 <div>
-                    <input type = "text" placeholder = "Nome" value = {name} onChange = {onNameChangeCbk}></input>
+                    <input type = "text" placeholder = "Nome" value = {name} onChange = {bind(setName)}></input>
                 </div>
                 <div>
-                    <input type = "text" placeholder = "Instruções" value = {instructions} onChange = {onInstructionsChangeCbk}></input>
+                    <input type = "text" placeholder = "Instruções" value = {instructions} onChange = {bind(setInstructions)}></input>
                 </div>
 
                 <div className = "Row">
@@ -535,7 +574,7 @@ function IngredientsTab()
     return (
         <div className="IngredientsTab">
             <h1> Ingredientes </h1>
-            <IngredientArea />
+            <IngredientsArea />
         </div>
     );
 }
